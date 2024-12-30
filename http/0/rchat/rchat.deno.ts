de4dea0ab65b03241c02c95be0748e4a88a27e03
samicpp <samichentouf+git2@gmail.com>
2024-12-30 16:35:25 +0100

    content change

---
+++ http/0/rchat/rchat.deno.ts
@@ -0,0 +1,89 @@
+//import docs from "../../../docs.d.ts";
+
+
+let del:Function;
+let visits=0;
+let td=new TextDecoder;
+let te=new TextEncoder;
+let closing=false;
+const room:WebSocket[]=[];
+//let active=true;
+console.log("rchat.deno.ts");
+
+async function handler(dest:number,ws:WebSocket,frame:WsFrame){
+    console.log("ws.deno.ts frame",frame);
+    if(frame.opname=="ping")ws.pong(frame.payload);
+    else if(frame.opname=="pong")console.log("rchat.deno.ts pong received");
+
+    else if(frame.opname=="text"){
+        console.log("rchat.deno.ts got something");
+        let r=room[dest]?.sendText(frame.payload);
+        console.log("rchat.deno.ts forward result",r);
+    }
+    else if(frame.opname=="close"&&!closing){
+        console.log("rchat.deno.ts client closing", td.decode(frame.close.message));
+        if(frame.close.code==-1){
+            console.log("rchat.deno.ts client didnt send any closure data");
+            ws.close(1002, "Client initiated closure. Cannot read closure code");
+        }
+        else ws.close(frame.close.code,"Client initiated closure");
+        del();
+    }
+    else if(frame.opname=="close"&&closing){
+        console.log("rchat.deno.ts close acknowledgement received");
+        ws.end();
+        del();
+    }
+}
+
+export default async function(socket: HttpSocket, url, get){
+    const {client}=socket;
+    console.log("rchat.deno.ts",1);
+
+    if(client.headers["upgrade"]=="websocket"){
+        console.log("rchat.deno.ts websocket upgrade");
+        let w=await socket.websocket();
+        if(w)console.log("rchat.deno.ts upgraded connection");
+        else return console.log("rchat.deno.ts couldnt upgrade");
+
+        w.on("frame",handler.bind(this,0,w));
+        room.push(w);
+
+        console.log("rchat.deno.ts",room);
+        room[0].sendText("test");
+        room[1].sendText("test");
+        del();
+    }else{
+        console.log("rchat.deno.ts client isnt trying to upgrade");
+        await socket.writeText("hello");
+        socket.close(); 
+    }
+}
+export async function init(socket: HttpSocket, url: URL, get: string, dele: ()=> void, past: any|void){
+    del=dele;
+    const {client}=socket;
+    console.log("rchat.deno.ts",0);
+    
+
+    if(client.headers["upgrade"]=="websocket"){
+        console.log("rchat.deno.ts websocket upgrade");
+        let w=await socket.websocket();
+        if(w)console.log("rchat.deno.ts upgraded connection");
+        else return console.log("rchat.deno.ts couldnt upgrade");
+
+        w.on("frame",handler.bind(this,1,w));
+        room.push(w);
+    }else{
+        console.log("rchat.deno.ts client isnt trying to upgrade");
+        await socket.writeText("hello");
+        socket.close(); 
+        dele();
+    }
+
+    //dele();
+}
+
+//export {active};
+export const state:{active:boolean}={
+    active:false,
+};
